d42aa36660a6001726b2df92fca599988a96ee58
juancruz0103 <juancruzjcv0@gmail.com>
2025-07-18 10:21:01 -0300

    mejoras final

--- front_end/src/layouts/OdontologoLayout.jsx
+++ front_end/src/layouts/OdontologoLayout.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
 import DashboardHeader from "../components/DashboardHeader"
-import "./AdminLayout.css" 
+import { logout } from "../auth/logout"
+import "./AdminLayout.css"
 
 export default function OdontologoLayout() {
   return (
     <div className="admin-layout">
       <Sidebar />
       <div className="admin-main">
-        <DashboardHeader title="Panel del Odontólogo" />
+        <DashboardHeader title="Panel del Odontólogo">
+          <button onClick={logout} className="logout-button">
+            Cerrar sesión
+          </button>
+        </DashboardHeader>
         <div className="admin-content">
           <Outlet />
         </div>
